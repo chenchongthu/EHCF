2098d903521ad201227ca164d7c18f988b2c4579
Chong Chen <cstchenc@163.com>
2020-02-16 20:09:37 +0800

    Update EHCF.py

--- code/EHCF.py
+++ code/EHCF.py
@@ -374,7 +374,7 @@ def dev_step(tset, train_m, test_m):
 
         recall = []
 
-        for kj in [10,50, 100]:
+        for kj in [50,100, 200]:
             idx_topk_part = np.argpartition(-pre, kj, 1)
 
             # print pre[np.arange(batch_users)[:, np.newaxis], idx_topk_part[:, :kj]]
